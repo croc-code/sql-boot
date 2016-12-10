49099e8259bedd844e2e2456a2af4cfa411a1664
mgramin <mgramin@gmail.com>
2016-12-10 10:43:10 +0300

    add postgres profile

--- src/main/java/com/github/mgramin/sqlboot/rest/Main.java
+++ src/main/java/com/github/mgramin/sqlboot/rest/Main.java
@@ -2,14 +2,17 @@ package com.github.mgramin.sqlboot.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication();
-		//application.setAdditionalProfiles("spring.profiles.active=information_schema");
+//		String profile = "postgres";
+		String profile = "information_schema";
 
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, profile);
+		SpringApplication application = new SpringApplication();
 		application.run(Main.class, args);
 	}
 
