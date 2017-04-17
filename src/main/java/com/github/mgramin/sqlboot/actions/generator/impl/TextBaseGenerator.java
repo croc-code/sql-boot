39b84d48a41c3275a2b7bf751cb2a94fb3411455
mgramin <mgramin@gmail.com>
2017-04-18 00:15:17 +0300

    add base generators

---
+++ src/main/java/com/github/mgramin/sqlboot/actions/generator/impl/TextBaseGenerator.java
@@ -0,0 +1,22 @@
+package com.github.mgramin.sqlboot.actions.generator.impl;
+
+import com.github.mgramin.sqlboot.actions.generator.IActionGenerator;
+import com.github.mgramin.sqlboot.exceptions.SqlBootException;
+import com.github.mgramin.sqlboot.model.DBSchemaObjectCommand;
+
+import java.util.Map;
+
+/**
+ * Created by maksim on 18.04.17.
+ */
+public class TextBaseGenerator implements IActionGenerator {
+    @Override
+    public String generate(Map<String, Object> variables) throws SqlBootException {
+        return null;
+    }
+
+    @Override
+    public DBSchemaObjectCommand command() {
+        return null;
+    }
+}
