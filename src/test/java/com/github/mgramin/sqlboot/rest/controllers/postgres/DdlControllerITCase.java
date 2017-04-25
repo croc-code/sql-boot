70344b8a7aa3a27b26d3e166073c669807276439
mgramin <mgramin@gmail.com>
2017-04-25 12:05:19 +0300

    use testcontainers instead maven docker plugin

--- src/test/java/com/github/mgramin/sqlboot/rest/controllers/postgres/DdlControllerITCase.java
+++ src/test/java/com/github/mgramin/sqlboot/rest/controllers/postgres/DdlControllerITCase.java
@@ -1,15 +1,22 @@
 package com.github.mgramin.sqlboot.rest.controllers.postgres;
 
 import com.github.mgramin.sqlboot.rest.RestRunner;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.EnvironmentTestUtils;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.GenericContainer;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,24 +26,37 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RestRunner.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(properties = {
-        "management.port=0", "db_dev_url=jdbc:postgresql://localhost:9876/postgres",
-        "db_dev_user=postgres", "db_dev_password=postgres" })
+@TestPropertySource(properties = {"db_dev_user=postgres", "db_dev_password=postgres"})
 @ActiveProfiles("postgres")
+@ContextConfiguration(initializers = DdlControllerITCase.Initializer.class)
 public class DdlControllerITCase {
 
     @Autowired
     private TestRestTemplate client;
 
+    @ClassRule
+    public static GenericContainer postgres = new GenericContainer("mgramin/postgres-sport:latest").withExposedPorts(5432);
+
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            EnvironmentTestUtils.addEnvironment("testcontainers", configurableApplicationContext.getEnvironment(),
+                    "db_dev_url=jdbc:postgresql://" + postgres.getContainerIpAddress() +
+                    ":" + postgres.getMappedPort(5432) + "/postgres"
+            );
+        }
+    }
 
     @Test
     public void getTextDdl() throws Exception {
+        Thread.currentThread().sleep(2000);
         callRestAndValidate("t/public.basketball_team_stats/drop", "postgres/t_public.basketball_team_stats_drop");
         callRestAndValidate("t/public.basketball_team_stats/d", "postgres/t_public.basketball_team_stats_drop");
         callRestAndValidate("t/public.basketball_team_stats/-", "postgres/t_public.basketball_team_stats_drop");
     }
 
-    private void callRestAndValidate(String uri, String file) throws IOException {
+    private void callRestAndValidate(String uri, String file) throws IOException, InterruptedException {
         ResponseEntity<String> response = client.getForEntity("/api/" + uri, String.class);
         assertEquals(response.getStatusCodeValue(), 200);
         String s = response.getBody();
