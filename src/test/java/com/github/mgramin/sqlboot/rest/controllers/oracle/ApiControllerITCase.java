84e5011bcbb46b4ec43dea79fe8020115a550b4a
mgramin <mgramin@gmail.com>
2017-04-26 01:09:39 +0300

    preparing config for oracle

---
+++ src/test/java/com/github/mgramin/sqlboot/rest/controllers/oracle/ApiControllerITCase.java
@@ -0,0 +1,68 @@
+package com.github.mgramin.sqlboot.rest.controllers.oracle;
+
+import com.github.mgramin.sqlboot.rest.RestRunner;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.EnvironmentTestUtils;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.GenericContainer;
+
+import java.io.File;
+import java.io.IOException;
+
+import static java.lang.String.format;
+import static org.apache.commons.io.FileUtils.readFileToString;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RestRunner.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"db_dev_user=system", "db_dev_password=oracle"})
+@ActiveProfiles("oracle")
+@ContextConfiguration(initializers = ApiControllerITCase.Initializer.class)
+public class ApiControllerITCase {
+
+    @Autowired
+    private TestRestTemplate client;
+
+    @ClassRule
+    public static GenericContainer oracle = new GenericContainer("sath89/oracle-xe-11g:latest")
+            .withExposedPorts(5432);
+
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            EnvironmentTestUtils.addEnvironment("testcontainers", configurableApplicationContext.getEnvironment(),
+                    format("db_dev_url= jdbc:oracle:thin:@%s:%s:%s",
+                            oracle.getContainerIpAddress(), oracle.getMappedPort(1521), "xe"));
+        }
+    }
+
+    @Test
+    public void getTextDdl() throws Exception {
+        Thread.sleep(2000);
+//        callRestAndValidate("t/public.basketball_team_stats/drop", "postgres/t_public.basketball_team_stats_drop");
+//        callRestAndValidate("t/public.basketball_team_stats/d", "postgres/t_public.basketball_team_stats_drop");
+//        callRestAndValidate("t/public.basketball_team_stats/-", "postgres/t_public.basketball_team_stats_drop");
+    }
+
+    private void callRestAndValidate(String uri, String file) throws IOException, InterruptedException {
+        ResponseEntity<String> response = client.getForEntity("/api/" + uri, String.class);
+        assertEquals(response.getStatusCodeValue(), 200);
+        String s = response.getBody();
+        String s1 = readFileToString(new File(getClass().getClassLoader().getResource(file).getFile()), "UTF-8");
+        System.out.println(s);
+        assertEquals(s1, s.trim());
+    }
+
+}
