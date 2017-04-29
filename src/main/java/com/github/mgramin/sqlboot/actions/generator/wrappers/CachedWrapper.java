100f4a2ddbbaa28d13f7dc4f7d92f108e4ee4860
mgramin <mgramin@gmail.com>
2017-04-29 22:00:59 +0300

    add stub for cached wrapper

---
+++ src/main/java/com/github/mgramin/sqlboot/actions/generator/wrappers/CachedWrapper.java
@@ -0,0 +1,24 @@
+package com.github.mgramin.sqlboot.actions.generator.wrappers;
+
+import com.github.mgramin.sqlboot.actions.generator.IActionGenerator;
+import com.github.mgramin.sqlboot.exceptions.SqlBootException;
+import com.github.mgramin.sqlboot.model.DBSchemaObjectCommand;
+
+import java.util.Map;
+
+/**
+ * Created by maksim on 29.04.17.
+ */
+public class CachedWrapper implements IActionGenerator {
+
+    @Override
+    public String generate(Map<String, Object> variables) throws SqlBootException {
+        throw new RuntimeException("Coming soon!");
+    }
+
+    @Override
+    public DBSchemaObjectCommand command() {
+        throw new RuntimeException("Coming soon!");
+    }
+
+}
