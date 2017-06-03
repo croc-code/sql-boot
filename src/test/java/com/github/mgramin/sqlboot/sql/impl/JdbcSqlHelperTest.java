010080f4aba8d81e1b5bafd03aba656ecee1dacc
mgramin <mgramin@gmail.com>
2017-06-03 11:14:24 +0300

    fix some checkstyle warnings

---
+++ src/test/java/com/github/mgramin/sqlboot/sql/impl/JdbcSqlHelperTest.java
@@ -0,0 +1,52 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2017 Maksim Gramin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.mgramin.sqlboot.sql.impl;
+
+import java.util.List;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by mgramin on 24.10.2016.
+ */
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = {"/test_config.xml"})
+public class JdbcSqlHelperTest {
+
+    @Autowired
+    private JdbcSqlHelper jdbcSqlHelper;
+
+    @Test
+    public void select() throws Exception {
+        List<Map<String, String>> select = jdbcSqlHelper.select("select * from (select name AS n, email as mail from main_schema.users)");
+        assertEquals(select.toString(), "[{N=mkyong, MAIL=mkyong@gmail.com}, {N=alex, MAIL=alex@yahoo.com}, {N=joel, MAIL=joel@gmail.com}]");
+    }
+
+}
