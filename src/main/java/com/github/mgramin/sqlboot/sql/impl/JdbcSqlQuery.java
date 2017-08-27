9591851660f5b4f5c6457f6c099b43488db18e2a
mgramin <mgramin@gmail.com>
2017-08-27 16:31:05 +0300

    fix #81

--- src/main/java/com/github/mgramin/sqlboot/sql/impl/JdbcSqlQuery.java
+++ src/main/java/com/github/mgramin/sqlboot/sql/impl/JdbcSqlQuery.java
@@ -3,42 +3,38 @@
  *
  * Copyright (c) 2016-2017 Maksim Gramin
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package com.github.mgramin.sqlboot.sql.impl;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
 import com.github.mgramin.sqlboot.exceptions.BootException;
 import com.github.mgramin.sqlboot.sql.SqlQuery;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
- * Execute SQL-query through Jdbc.
+ * Execute SQL-query through plain old Jdbc.
  *
  * @author Maksim Gramin (mgramin@gmail.com)
  * @version $Id$
@@ -62,35 +58,17 @@ public final class JdbcSqlQuery implements SqlQuery {
 
     @Override
     public List<Map<String, String>> select(final String sql)
-    throws BootException {
-        final List<Map<String, String>> result = new ArrayList<>();
-        try (Connection connection = dataSource.getConnection()) {
-            final Statement statement = connection.createStatement();
-            try (ResultSet resultSet = statement.executeQuery(sql)) {
-                final ResultSetMetaData rsMetaData = resultSet.getMetaData();
-                final int columnCount = rsMetaData.getColumnCount();
-                while (resultSet.next()) {
-                    final Map<String, String> map = new LinkedHashMap<>();
-                    for (int i = 1; i < columnCount + 1; i++) {
-                        map.put(rsMetaData.getColumnName(i),
-                                resultSet.getString(i));
-                    }
-                    result.add(map);
-                }
-            }
-        } catch (SQLException e) {
-            throw new BootException("SQL Exception", e);
-        }
-        return result;
+        throws BootException {
+        return new JdbcTemplate(dataSource).queryForList(sql).stream()
+            .map(map -> map.entrySet().stream()
+                .collect(toMap(Entry::getKey, v -> v.getValue().toString(), (a, b) -> a,
+                LinkedHashMap::new)))
+            .collect(toList());
     }
 
     @Override
     public void dbHealth() {
-        try {
-            dataSource.getConnection();
-        } catch (SQLException e) {
-            throw new BootException(e);
-        }
+        new JdbcTemplate(dataSource);
     }
 
 }
