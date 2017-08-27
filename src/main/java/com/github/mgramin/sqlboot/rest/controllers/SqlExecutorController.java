4fa7189cd5ee68aba82cd78d073775834f145317
mgramin <mgramin@gmail.com>
2017-08-27 16:48:00 +0300

    fix #76

--- src/main/java/com/github/mgramin/sqlboot/rest/controllers/SqlExecutorController.java
+++ src/main/java/com/github/mgramin/sqlboot/rest/controllers/SqlExecutorController.java
@@ -3,23 +3,20 @@
  *
  * Copyright (c) 2016-2017 Maksim Gramin
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package com.github.mgramin.sqlboot.rest.controllers;
@@ -31,7 +28,6 @@ import java.util.Map;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,30 +39,35 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by mgramin on 31.12.2016.
  */
 @RestController
-@ComponentScan(basePackages = "com.github.mgramin.sqlboot")
 @EnableAutoConfiguration
 public final class SqlExecutorController {
 
     @Autowired
     DataSource dataSource;
 
-    @RequestMapping(value = "exec", produces = {MediaType.APPLICATION_XML_VALUE})
-    public List<Map<String, String>> execSql2Xml(@RequestParam("sql") String sql) throws BootException {
+    @RequestMapping(value = "sql", produces = {MediaType.APPLICATION_XML_VALUE})
+    public List<Map<String, String>> execSql2Xml(@RequestBody String sql)
+        throws BootException {
         return new JdbcSqlQuery(dataSource).select(sql);
     }
 
-    @RequestMapping(value = "exec", method = RequestMethod.POST, produces = {MediaType.APPLICATION_XML_VALUE})
-    public List<Map<String, String>> execSql2XmlPost(@RequestBody() String sql) throws BootException {
+    @RequestMapping(value = "exec", method = RequestMethod.POST, produces = {
+        MediaType.APPLICATION_XML_VALUE})
+    public List<Map<String, String>> execSql2XmlPost(@RequestBody() String sql)
+        throws BootException {
         return new JdbcSqlQuery(dataSource).select(sql);
     }
 
     @RequestMapping(value = "exec", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<Map<String, String>> execSql2Json(@RequestParam("sql") String sql) throws BootException {
+    public List<Map<String, String>> execSql2Json(@RequestParam("sql") String sql)
+        throws BootException {
         return new JdbcSqlQuery(dataSource).select(sql);
     }
 
-    @RequestMapping(value = "exec", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<Map<String, String>> execSql2JsonPost(@RequestBody() String sql) throws BootException {
+    @RequestMapping(value = "exec", method = RequestMethod.POST, produces = {
+        MediaType.APPLICATION_JSON_VALUE})
+    public List<Map<String, String>> execSql2JsonPost(@RequestBody() String sql)
+        throws BootException {
         return new JdbcSqlQuery(dataSource).select(sql);
     }
 
--- src/test/java/com/github/mgramin/sqlboot/rest/controllers/SqlExecutorControllerITCase.java
+++ src/test/java/com/github/mgramin/sqlboot/rest/controllers/SqlExecutorControllerITCase.java
@@ -3,23 +3,20 @@
  *
  * Copyright (c) 2016-2017 Maksim Gramin
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package com.github.mgramin.sqlboot.rest.controllers;
@@ -53,9 +50,8 @@ public class SqlExecutorControllerITCase {
     @Test
     @Ignore
     public void execSql2Xml() throws Exception {
-        ResponseEntity<String> forEntity = this.restClient.exchange(
-                "/exec?sql=select 1 as one, 2 as two", HttpMethod.GET, null, String.class);
-
+        final ResponseEntity<String> forEntity = this.restClient.exchange(
+            "/sql", HttpMethod.GET, new HttpEntity<>("select 1 as one, 2 as two"), String.class);
         assertEquals(forEntity.getBody(), "[{\"ONE\":\"1\",\"TWO\":\"2\"}]");
         assertEquals(forEntity.getHeaders().getContentType(), MediaType.APPLICATION_JSON_UTF8);
     }
@@ -63,8 +59,8 @@ public class SqlExecutorControllerITCase {
     @Test
     @Ignore
     public void execSql2Json() throws Exception {
-        ResponseEntity<String> forEntity = this.restClient.exchange(
-                "/exec?sql=select 1 as one, 2 as two", HttpMethod.GET, null, String.class);
+        final ResponseEntity<String> forEntity = this.restClient.exchange(
+            "/sql", HttpMethod.GET, new HttpEntity<>("select 1 as one, 2 as two"), String.class);
 
         assertEquals(forEntity.getBody(), "[{\"ONE\":\"1\",\"TWO\":\"2\"}]");
         assertEquals(forEntity.getHeaders().getContentType(), MediaType.APPLICATION_JSON_UTF8);
@@ -74,10 +70,11 @@ public class SqlExecutorControllerITCase {
     @Ignore
     public void execSql2JsonPost() throws Exception {
         HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+//        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<>("select 1 as one, 2 as two", headers);
 
-        ResponseEntity<String> forEntity = restClient.exchange("/exec", HttpMethod.POST, entity, String.class);
+        ResponseEntity<String> forEntity = restClient
+            .exchange("/sql", HttpMethod.POST, entity, String.class);
         System.out.println(forEntity.getBody());
 
         assertEquals(forEntity.getBody(), "[{\"ONE\":\"1\",\"TWO\":\"2\"}]");
