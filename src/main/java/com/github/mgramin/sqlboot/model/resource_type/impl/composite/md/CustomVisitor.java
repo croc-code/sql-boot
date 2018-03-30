3d60318e989079e92226bd7db58ffc59f676f97a
mgramin <mgramin@gmail.com>
2018-03-31 00:13:34 +0300

    fix #104 (#157)
    * Create branch #104
    
    * add md visitor
    
    * add md visitor
    
    * save register for headers element

---
+++ src/main/java/com/github/mgramin/sqlboot/model/resource_type/impl/composite/md/CustomVisitor.java
@@ -0,0 +1,58 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2017 Maksim Gramin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.mgramin.sqlboot.model.resource_type.impl.composite.md;
+
+import org.commonmark.node.AbstractVisitor;
+import org.commonmark.node.FencedCodeBlock;
+import org.commonmark.node.Heading;
+import org.commonmark.node.Text;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CustomVisitor extends AbstractVisitor {
+
+    private String currentTag;
+    private Map<String, String> map = new LinkedHashMap<>();
+
+    @Override
+    public void visit(Text text) {
+        if (text.getParent() instanceof Heading && ((Heading)text.getParent()).getLevel()>=3) {
+            currentTag = text.getLiteral();
+        }
+    }
+
+    @Override
+    public void visit(FencedCodeBlock fencedCodeBlock) {
+        if (fencedCodeBlock.getFenceLength() == 4) {
+            map.put(currentTag, fencedCodeBlock.getLiteral());
+        }
+    }
+
+    public Map<String, String> getMap() {
+        return map;
+    }
+
+}
