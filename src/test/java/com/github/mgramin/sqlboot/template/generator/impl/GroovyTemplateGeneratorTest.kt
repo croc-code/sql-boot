908b11f5c398fc2787988bfabf81fdfbc64b8bc7
mgramin <mgramin@gmail.com>
2018-12-26 04:29:07 +0400

    kotlinze one more test

---
+++ src/test/java/com/github/mgramin/sqlboot/template/generator/impl/GroovyTemplateGeneratorTest.kt
@@ -0,0 +1,62 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2017 Maksim Gramin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.mgramin.sqlboot.template.generator.impl
+
+import com.github.mgramin.sqlboot.exceptions.BootException
+import com.google.common.collect.ImmutableMap.of
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+/**
+ * Created by MGramin on 10.12.2016.
+ */
+class GroovyTemplateGeneratorTest {
+
+    @Test
+    @Throws(BootException::class)
+    fun process() {
+
+        val txt = """... where lower(c.table_schema) like '${'$'}schema'
+                    |      and lower(c.table_name) like '${'$'}table'
+                    |      and lower(c.column_name) like '${'$'}column'""".trimMargin()
+
+        val result = """... where lower(c.table_schema) like 'public'
+                       |      and lower(c.table_name) like 'persons'
+                       |      and lower(c.column_name) like 'id'""".trimMargin()
+
+        val maps = of<String, Any>("column", "id", "table", "persons", "schema", "public")
+        val templateGenerator = GroovyTemplateGenerator(txt)
+        assertEquals(templateGenerator.generate(maps), result)
+    }
+
+    @Test
+    @Throws(BootException::class)
+    fun processLoweCase() {
+        val maps = of<String, Any>("column", "id", "table", "persons", "schema", "public")
+        val templateGenerator = GroovyTemplateGenerator("create table \${table.toLowerCase()} ...")
+        assertEquals(templateGenerator.generate(maps), "create table persons ...")
+    }
+
+}
