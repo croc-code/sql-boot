a71620d8e1ef7d3d56afdfb21379780e3ead29c9
mgramin <mgramin@gmail.com>
2019-01-05 22:42:41 +0300

    #198

---
+++ src/main/java/com/github/mgramin/sqlboot/tools/files/file_system/impl/LocalFileSystem.kt
@@ -0,0 +1,57 @@
+/*
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2016-2019 Maksim Gramin
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.mgramin.sqlboot.tools.files.file_system.impl
+
+import com.github.mgramin.sqlboot.tools.files.file.File
+import com.github.mgramin.sqlboot.tools.files.file.impl.SimpleFile
+import com.github.mgramin.sqlboot.tools.files.file_system.FileSystem
+import org.apache.commons.io.FileUtils
+import org.apache.tools.ant.DirectoryScanner
+import java.util.*
+
+/**
+ * @author Maksim Gramin (mgramin@gmail.com)
+ * @version $Id: eed285888c2d7886f998876c554a6d8f51c6a501 $
+ * @since 0.1
+ */
+class LocalFileSystem(private val basedir: String) : FileSystem {
+
+    override fun listFiles(mask: String): List<File> {
+        val scanner = DirectoryScanner()
+        scanner.setIncludes(arrayOf(mask))
+        scanner.setBasedir(this.basedir.replace("\\", "/"))
+        scanner.isCaseSensitive = false
+        scanner.scan()
+        val files = scanner.includedFiles
+        val result = ArrayList<File>()
+        for (file in files) {
+            result.add(
+                    SimpleFile(file,
+                            FileUtils.readFileToByteArray(java.io.File("$basedir/$file"))))
+        }
+        return result
+    }
+
+}
