2d7d285eaa6226af030dc59ab1bc0e709eac1775
mgramin <mgramin@gmail.com>
2019-03-16 10:22:49 +0300

    #269

---
+++ src/test/kotlin/com/github/mgramin/sqlboot/model/connection/DbConnectionListTest.kt
@@ -0,0 +1,63 @@
+/*
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2016-2019 Maksim Gramin
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.mgramin.sqlboot.model.connection
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@EnableAutoConfiguration
+@ContextConfiguration(classes = [DbConnectionList::class], initializers = [ConfigFileApplicationContextInitializer::class])
+internal class DbConnectionListTest {
+
+    @Autowired
+    lateinit var dbConnectionList: DbConnectionList
+
+    @ParameterizedTest
+    @ValueSource(strings = ["test", "dev", "prod"])
+    fun getConnectionByName(connectionName: String) {
+        assertEquals(connectionName, dbConnectionList.getConnectionByName(connectionName).getName())
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["test", "dev", "prod"])
+    fun getConnectionsByMask(connectionMask: String) {
+        assertEquals(connectionMask, dbConnectionList.getConnectionsByMask(connectionMask).first().getName())
+    }
+
+    @Test
+    fun getConnections() {
+        assertEquals(4, dbConnectionList.connections.count())
+    }
+
+}
